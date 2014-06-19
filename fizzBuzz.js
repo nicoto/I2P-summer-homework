1c0c9621cb5d8f49312f9c0c2190e4bdd5a0352d
OpenShift guest <nicholas_toulouse@hotmail.com>
2014-06-19 15:24:36 -0400

    minor error fixes to fizzBuzz

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,11 +1,11 @@
-for(count=1; count<=15; count++) {
-    if(count%3 == 0 && count%5 == 0) {
+for(var count=1; count<=15; count++) {
+    if(count%3 === 0 && count%5 === 0) {
      console.log('FizzBuzz');
     }
-    else if(count%3 == 0) {
+    else if(count%3 === 0) {
         console.log('Fizz');
     } 
-    else if(count%5 == 0){
+    else if(count%5 === 0){
      console.log('Buzz');
     }
     else {
