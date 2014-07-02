7b6b79269dbe45ab9f79c3910a0c2f0b27937dc3
OpenShift guest <nicholas_toulouse@hotmail.com>
2014-07-02 17:05:43 -0400

    hw3 work in progress

--- js/hw3_todolist.js
+++ js/hw3_todolist.js
@@ -1,48 +1,54 @@
-
-var due = new Date();
-var task1 = {
-    taskName:"Clean room",
-    dueDate:due,
-    statusOfDone:true
-};
-var task2 = {
-    taskName:"Grocery shop",
-    dueDate:due,
-    statusOfDone:false
-};
-var task3 = {
-    taskName:"Get an oil change",
-    dueDate:due,
-    //statusOfDone:false
-};
-var task4 = {
-    taskName:"Haircut",
-    dueDate:due,
-    statusOfDone:false
-};
+    var toDoList = [
+    {'name': 'Clean room', 'complete' : true},
+    {'name': 'Grocery shop', 'complete' : true},
+    {'name': 'Get an oil change', 'complete' : false},
+    {'name': 'Do course homework', 'complete' : true},
+    {'name': 'Get a haircut', 'complete' : false}
+    ];
     
+$(document).ready(function() {
 
-var toDoList = [task1, task2, task3, task4];
+    // Create the unordered list element
+    $(".panel-body").add("ul").addClass("toDoTaskList");
 
-document.write("<ul class='toDoTaskList'>");
-
-for(var i=0; i in toDoList; i++) {
-
-if(toDoList[i].taskName!==null&&toDoList[i].dueDate!==null&&toDoList[i].statusOfDone!==null){
-document.writeln("<li class='taskItem'>Task to do? <i>" + toDoList[i].taskName + "</i> When's it due? <i>" + toDoList[i].dueDate + "</i> Is it done? <i>" + toDoList[i].statusOfDone + "</i></li>");  
-}
-else
-throw "One of the tasks in your todo list is incomplete. See: " + toDoList[i];
-}
-
-/*try {
-toDoList[i].taskName;
-toDoList[i].dueDate;
-toDoList[i].statusOfDone;
-return false;    
-}
-catch(exception) {
-return true;
-}*/
-
-document.write("</ul>");
+    for(var i=0; i in toDoList; i++) {
+    try{
+        printTask(toDoList[i], i);
+    }
+    catch(error) {
+        console.log("One of the tasks in your to-do-list is incomplete; " + error);
+    }
+    }
+    
+    function printTask(taskObj, counter) {
+        var $lis = $( "li" );
+        console.log(taskObj.name === undefined);
+        if(taskObj.name === undefined) {
+            throw "Alas, what cannot be identified or named, can hardly be done.";
+        }
+        console.log(taskObj);
+        if(taskObj['complete']) { // or you can write if(taskObj.complete === true) / taskObj['complete']
+            $(".toDoTaskList").append("<li class='taskItem' id='litem-" + counter + "'></li>");
+            $lis.eq( counter ).html(taskObj.name + '&nbsp;&nbsp;<i class="glyphicon glyphicon-ok"></i>');
+            //$('#litem'+counter).html(taskObj.name + '&nbsp;&nbsp;<i class="glyphicon glyphicon-ok"></i>');
+            $lis.eq( counter ).css( "color", "green" );
+            
+            //document.getElementById(lid).textContent = taskObj.name + '&nbsp;&nbsp;<i class="glyphicon glyphicon-ok"></i>';
+            // HW2: document.write('<li class="taskItem">' + taskObj.name + '&nbsp;&nbsp;<i class="glyphicon glyphicon-ok"></i></li>');
+        }
+        else {
+            $(".toDoTaskList").append("<li class='taskItem' id='litem-" + counter + "'></li>");
+            $lis.eq( counter ).html(taskObj.name);
+            //$("#litem"+counter).html(taskObj.name);
+            //document.getElementById(lid).textContent = taskObj.name + '&nbsp;&nbsp;<i class="glyphicon glyphicon-ok"></i>';
+            // HW2: document. write('<li class="taskItem">'+ taskObj.name + '</li>');
+        }
+    }
+    
+    function removeTask(taskObj, index) {
+        $.inArray(taskObj)
+    }
+    // JQUERY HERE TO GENERATE HTML
+    // $(".toDoTaskList").append("<li class='taskItem'></li>");
+    // $('.taskItem').text('abc');
+});
