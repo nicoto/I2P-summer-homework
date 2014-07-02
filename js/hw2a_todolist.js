caeb07152b1d0fc8ba6a9ec96903e7add4ed6ea0
OpenShift guest <nicholas_toulouse@hotmail.com>
2014-07-02 12:39:49 -0400

    Final homework #2 correct error handling output

--- js/hw2a_todolist.js
+++ js/hw2a_todolist.js
@@ -24,7 +24,7 @@ var toDoList = [task1, task2, task3, task4];
 */
 
 var toDoList = [
-    {'name': 'Clean room', 'complete' : true},
+    {/*'name': 'Clean room', */'complete' : true},
     {'name': 'Grocery shop', 'complete' : true},
     {'name': 'Get an oil change', 'complete' : false},
     {'name': 'Do course homework', 'complete' : true},
@@ -38,22 +38,16 @@ for(var i=0; i in toDoList; i++) {
         printTask(toDoList[i]);
    }
    catch(error) {
-        console.log("One of the items in your tasks object is incomplete" + error);
+        console.log("One of the tasks in your to-do-list is incomplete; " + error);
    }
-
-//if(toDoList[i].taskName!==null&&toDoList[i].dueDate!==null&&toDoList[i].statusOfDone!==null){
-//document.writeln("<li class='taskItem'>Task to do? <i>" + toDoList[i].taskName + "</i> When's it due? <i>" + toDoList[i].dueDate + "</i> Is it done? <i>" + toDoList[i].statusOfDone + "</i></li>");  
-//}
-//else
-//throw "One of the tasks in your todo list is incomplete. See: " + toDoList[i];
 }
 
 function printTask(taskObj) {
-    console.log(taskObj.name === null);
-    if(taskObj.name===null) {
-        throw "Alas, what cannot be named, cannot be done.";
+    //console.log(taskObj.name === undefined);
+    if(taskObj.name === undefined) {
+        throw "Alas, what cannot be identified or named, can hardly be done.";
     }
-    console.log(taskObj);
+    //console.log(taskObj);
     if(taskObj['complete']) { // or you can write if(taskObj.complete === true) / taskObj['complete']
         document.write('<li class="taskItem">' + taskObj.name + '&nbsp;&nbsp;<i class="glyphicon glyphicon-ok"></i></li>');
     }
