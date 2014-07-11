199ed1b1f976689c7e55ec639436f891456d2868
nicoto <nicholas_toulouse@hotmail.com>
2014-07-11 21:02:08 +0000

    final corrected version of the TODO app for HW #4

--- js/hw4_todolist.js
+++ js/hw4_todolist.js
@@ -1,28 +1,34 @@
-$(document).ready(function () {
-
-    // EQUIVALENT TO THE MAIN PROGRAM FUNCTION
+//Needs to be a global variable
+    var tempDate = new Date();
+    
     var toDoList = [{
         'name': 'Clean room',
+        'dueDate': tempDate,
         'complete': true
     }, {
         'name': 'Grocery shop',
+        'dueDate': tempDate,       
         'complete': true
     }, {
         'name': 'Get an oil change',
+        'dueDate': tempDate,         
         'complete': false
     }, {
         'name': 'Do course homework',
+        'dueDate': tempDate,         
         'complete': true
     }, {
         'name': 'Get a haircut',
+        'dueDate': tempDate,         
         'complete': false
     }];
+    
+$(document).ready(function () {
 
     // Unordered list element of class ***taskListGroup***
     $('.panel-body').append('<ul class="taskListGroup"></ul>');
 
-    addTask("Go running"); // Function call addTask calls newTask that calls dueNextWeek
-
+    // Some error handling with a call to output tasks into HTML
     for (var i = 0; i in toDoList; i++) {
         try {
             printTask(toDoList[i], i);
@@ -32,8 +38,8 @@ $(document).ready(function () {
     }
 
     function printTask(taskObj, i) {
-
-        // This conditional makes the counter argument passed into the function, as called, optional. Not this as originally written: if (counter===null) counter=0; 
+        console.log(taskObj);
+        // This conditional makes the counter argument passed into the function, as called, optional. Not this was originally written: if (counter===null) counter=0; 
         var counter = i || 0;
 
         if (taskObj.name === undefined) {
@@ -53,33 +59,27 @@ $(document).ready(function () {
     /* The function (addTask) calls newTask() and adds the object returned from that function to the array 
     where we keep the rest of our task objects. Once done, we re-render the list HTML.*/
     function addTask(needs2Bdone) {
-        var theTask = {};
-        theTask = newTask(needs2Bdone);
-        console.log("The task at hand is: " + theTask.name + ", " + theTask.dueDate + ", " + theTask.complete);
-        toDoList.pop(theTask);
-        printTask(toDoList);
 
-        // Now re-render the list in HTML
+        var theTask = new Task(needs2Bdone); // constructor call
+        console.log("The task at hand is: " + theTask.name + ", " + theTask.dueDate + ", " + theTask.complete);
+        toDoList.push(theTask); // adds the object to the ToDoListarray
+        console.log("You successfully added the new task object to your array.");
+        printTask(theTask);
 
     }
 
-    /*Write a constructor function (Task) that accepts a name and returns an object with properties for the name, 
-    complete status, and now a “due date”. For simplicity’s sake, every object’s complete status should be false, 
-    and every object’s due date should be a Javascript Date object (not a string) that is equal to one week from 
-    today.*/
-    function newTask(name) { // a constructor function
+   function Task(name) { // a constructor function
         var todaysDate = new Date();
         this.dueDate = dueNextWeek(todaysDate);
-        this.name = name;
-        this.complete = false;
+        console.log(this.dueDate); //OUTPUT to determine whether dueNextWeek runs correctly
+        this.name = name || 'test'; // this statement indicates an optional constructor function parameter
+        this['complete'] = false; // rather than this.complete because complete is a key word.
         var taskObj = {
             'name': this.name,
             'dueDate': this.dueDate,
             'complete': this.complete
         };
-        console.log(taskObj);
-        // Test to see if the object toDoList exists and whether or not it might be empty.
-        // Does the array exist, written as so: if (typeof toDoList == "undefined"){}
+        // Later, you may test to see if the object toDoList exists and whether or not it might be empty. Does the array exist, written as so: if (typeof toDoList == "undefined"){}
         return taskObj;
     }
 
@@ -88,6 +88,9 @@ $(document).ready(function () {
         console.log("One week from today is " + dueDate);
         return dueDate;
     }
+
+    // Function call
+    addTask("Go running");
 
     function removeCompleted() { //slice and splice
             $('li.taskCompleted').remove(); // just to affect the HTML of the page and the task object can stay the same.
